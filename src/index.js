7f670695d9e439a5e3a81739b1e0e7c7ac48db42
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-06-08 22:16:31 +0200

    basic constant
    -trials to write fetch function to get the countries from external HTTP
    -catching the Promise error
    -trying to GET the promise object when reponse is ok
    -added loadash library, notiflix
    -defined basic constants of the HTML, debounce delay value
    -added input listener on searchBox
    - basic requirement: to write renderCountryList function to get the successful country object from fetch function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+// imports
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+//Notiflix.Notify.failure("Oops, there is no country with that name");
 
+console.log(fetchCountries());
+
+//loadash library
+var debounce = require('lodash.debounce');
+
+//HTML selectors & constants
 const DEBOUNCE_DELAY = 300;
+const countryList = document.querySelector('.country-list');
+const searchBox = document.querySelector('#search-box');
+
+// input listener & its handleEvent & debounce delay
+searchBox.addEventListener(
+  'input',
+  debounce(handleSearchEvent, DEBOUNCE_DELAY)
+);
+
+function handleSearchEvent() {
+  fetchCountries()
+    .then(name => renderCountryList(name))
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
+}
