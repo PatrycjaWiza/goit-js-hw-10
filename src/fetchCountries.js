cc55c7a3c0bbfee0166ed4450d905189ead62e45
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-06-13 00:00:50 +0200

    update
    css for:
    1 result
    >2 <10 results
    -fix trim() to work on spaces
    -fix clearing countryList when spaces sonly (erased results)
    -get languages object from promise

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,7 @@
 const searchBox = document.querySelector('#search-box');
-
 function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v2/name/${searchBox.value}?limit=10,fields=name,capital,population,flag,languages`
+    `https://restcountries.com/v2/name/${searchBox.value}?fields=name,capital,population,flag,languages`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -22,27 +22,34 @@ function handleSearchEvent() {
 }
 // render countries to HTML
 function renderCountryList(name) {
+  // !!! SANITIZE INPUT VALUE TRIM() !!! 3of3
+  if (name.length > 10) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
   const markup = name
     .map(country => {
-      if (name.length > 10) {
-        return Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (name.length === 1) {
+      if (name.length === 1) {
         return `<li>      
-        <img src="${country.flag}">
-        <p>${country.name}</p>
+        <img src="${country.flag}" height=50px>
+        <h2>${country.name}</h2>
         <p><b>Capital:</b> ${country.capital}</p>
         <p><b>Population:</b> ${country.population}</p>
         <p><b>Languages:</b> ${country.languages}</p>
         </li>`;
+        // !!! FIX LANGUAGES !!! 2of3
       } else {
-        return `<li>        
-        <img src="${country.flag}">
+        return `<li style="display: flex;">        
+        <img src="${country.flag}" width= 70px>
         <p>${country.name}</p>
         </li>`;
       }
     })
     .join('');
   countryList.innerHTML = markup;
+  if (name.length < 1) {
+    return (countryList.innerHTML = '');
+  }
+  // !!! FIX CLEAR COUNTRYLIST !!! 1of3
 }
